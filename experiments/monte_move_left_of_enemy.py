369b1ac2a3e8a3c0567b9474d9b40fa1c3f4561b
Anita de Mello Koch <demellokochanita@gmail.com>
2022-09-19 11:51:38 -0400

    readd if main

--- experiments/monte_move_left_of_enemy.py
+++ experiments/monte_move_left_of_enemy.py
@@ -216,81 +216,83 @@ bootstrap_env = MonteBootstrapWrapper(
     check_termination_correct,
     agent_space=True
 )
+
+if __name__ == "__main__":
     
-parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-parser.add_argument("--base_dir", type=str, required=True)
-parser.add_argument("--seed", type=int, required=True)
-parser.add_argument("--config_file", nargs='+', type=str, required=True)
-parser.add_argument("--gin_bindings", default=[], help='Gin bindings to override the values' + 
-        ' set in the config files (e.g. "DQNAgent.epsilon_train=0.1",' +
-        ' "create_atari_environment.game_name="Pong"").')
+    parser.add_argument("--base_dir", type=str, required=True)
+    parser.add_argument("--seed", type=int, required=True)
+    parser.add_argument("--config_file", nargs='+', type=str, required=True)
+    parser.add_argument("--gin_bindings", default=[], help='Gin bindings to override the values' + 
+            ' set in the config files (e.g. "DQNAgent.epsilon_train=0.1",' +
+            ' "create_atari_environment.game_name="Pong"").')
+    
+    args = parser.parse_args()
 
-args = parser.parse_args()
+    load_gin_configs(args.config_file, args.gin_bindings)
 
-load_gin_configs(args.config_file, args.gin_bindings)
+    experiment = Experiment(
+        base_dir=args.base_dir,
+        seed=args.seed,
+        policy_phi=phi,
+        experiment_env_function=make_env,
+        get_percentage_function=get_percent_completed,
+        check_termination_true_function=check_termination_correct,
+        policy_bootstrap_env=bootstrap_env,
+        initiation_positive_files=initiation_positive_files,
+        initiation_negative_files=initiation_negative_files,
+        initiation_priority_negative_files=initiation_priority_negative_files,
+        termination_positive_files=termination_positive_files,
+        termination_negative_files=termination_negative_files,
+        termination_priority_negative_files=termination_priority_negative_files
+    )
 
-experiment = Experiment(
-    base_dir=args.base_dir,
-    seed=args.seed,
-    policy_phi=phi,
-    experiment_env_function=make_env,
-    get_percentage_function=get_percent_completed,
-    check_termination_true_function=check_termination_correct,
-    policy_bootstrap_env=bootstrap_env,
-    initiation_positive_files=initiation_positive_files,
-    initiation_negative_files=initiation_negative_files,
-    initiation_priority_negative_files=initiation_priority_negative_files,
-    termination_positive_files=termination_positive_files,
-    termination_negative_files=termination_negative_files,
-    termination_priority_negative_files=termination_priority_negative_files
-)
+    # experiment.save()
 
-# experiment.save()
-
-experiment.bootstrap_from_room(
-    load_init_states(initiation_state_files[0]),
-    terminations[0],
-    50,
-    use_agent_space=True
-)
-
-for y in range(len(initiation_state_files)):
-    idx = order[y]
-    experiment.run_trial(
-        load_init_states(initiation_state_files[idx]),
-        terminations[idx],
+    experiment.bootstrap_from_room(
+        load_init_states(initiation_state_files[0]),
+        terminations[0],
         50,
-        eval=True,
-        trial_name="{}_eval_after_bootstrap".format(room_names[idx]),
         use_agent_space=True
     )
 
-# experiment.save(additional_path=room_names[0])
-
-for x in range(1, len(initiation_state_files)):
-    idx = order[x]
-    experiment.run_trial(
-        load_init_states(initiation_state_files[idx]),
-        terminations[idx],
-        100,
-        eval=False,
-        trial_name="{}_train".format(room_names[idx]),
-        use_agent_space=True
-    )
     for y in range(len(initiation_state_files)):
-        idy = order[y]
+        idx = order[y]
         experiment.run_trial(
-            load_init_states(initiation_state_files[idy]),
-            terminations[idy],
+            load_init_states(initiation_state_files[idx]),
+            terminations[idx],
             50,
             eval=True,
-            trial_name="{}_eval_after_{}_train".format(room_names[idy], room_names[idx]),
+            trial_name="{}_eval_after_bootstrap".format(room_names[idx]),
             use_agent_space=True
         )
+
+    # experiment.save(additional_path=room_names[0])
+
+    for x in range(1, len(initiation_state_files)):
+        idx = order[x]
+        experiment.run_trial(
+            load_init_states(initiation_state_files[idx]),
+            terminations[idx],
+            100,
+            eval=False,
+            trial_name="{}_train".format(room_names[idx]),
+            use_agent_space=True
+        )
+        for y in range(len(initiation_state_files)):
+            idy = order[y]
+            experiment.run_trial(
+                load_init_states(initiation_state_files[idy]),
+                terminations[idy],
+                50,
+                eval=True,
+                trial_name="{}_eval_after_{}_train".format(room_names[idy], room_names[idx]),
+                use_agent_space=True
+            )
+        
+        # experiment.save(additional_path=room_names[idx])
+
     
-    # experiment.save(additional_path=room_names[idx])
-
-
-
+    
 
