f3b30011dc46cea967ed2847a6b8f56156018dbd
Bingnan Huo <98042550+BingnanHuo@users.noreply.github.com>
2024-01-17 12:02:25 -0500

    Old classifier progressive train experiment. Archived since using new classifier.

--- experiments/classifier/core/classifier_experiment.py
+++ experiments/classifier/core/classifier_experiment.py
@@ -1,11 +1,13 @@
 import datetime
 import random
 import os
+from re import I
 import gin
 import logging
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from pyparsing import List
 import torch
 
 from portable.option.sets.attention_set import AttentionSet
@@ -22,6 +24,7 @@ class ClassifierExperiment():
                  seed,
                  experiment_name,
                  classifier_train_epochs,
+                 num_testsets,
                  use_gpu=True):
         
         random.seed(seed)
@@ -43,8 +46,10 @@ class ClassifierExperiment():
                             level=logging.INFO)
         
         if use_gpu:
+            self.use_gpu = True
             self.embedding = AutoEncoder().to("cuda")
         else:
+            self.use_gpu = False
             self.embedding = AutoEncoder()
 
         self.embedding_loaded = False
@@ -53,41 +58,68 @@ class ClassifierExperiment():
                                        embedding=self.embedding,
                                        log_dir=self.log_dir)
         self.classifier_train_epochs = classifier_train_epochs
-        self.test_dataset = SetDataset()
+        
+        self.test_sets = []
+        for _ in range(num_testsets):
+            self.test_sets.append(SetDataset(attention_num=self.classifier.attention_num))
+
+        # full dataset (train + test). TODO: write fine_tune on full set, and retrain new classifier completely
+        #self.full_set = SetDataset(attention_num=self.classifier.attention_num)
 
     def add_train_data(self, true_data_files, false_data_files):
         # add data to attention set
         self.classifier.add_data_from_files(true_data_files, 
                                             false_data_files)
+        logging.info(f'ADDING TRAIN & VAL DATA...')
         logging.info(
-            "Added {} true files and {} false files to attention set.".format(
+            "Added {} true files and {} false files to classifier training set.".format(
                 len(true_data_files), len(false_data_files))
             )
         logging.info(
-            "Actual attention set dataset size: {} true files and {} false files".format(
+            "TRAIN size: {} true images and {} false images".format(
                 len(self.classifier.dataset.true_data), len(self.classifier.dataset.false_data))
             )
+        logging.info(
+            "VAL size: {} true images and {} false images".format(
+                len(self.classifier.dataset.validate_indicies_true), len(self.classifier.dataset.validate_indicies_false))
+            )
+        
+        '''self.full_set.add_true_files(true_data_files)
+        self.full_set.add_false_files(false_data_files)
+        logging.info(
+            "Full dataset size: {} true images and {} false images".format(
+                len(self.full_set.true_data), len(self.full_set.false_data))
+            )'''
+        
 
-    def add_test_data(self, true_data_files, false_data_files):
-        # add data to test set
+    def add_test_data(self, test_set_idx, true_data_files, false_data_files):
+        # add data to the given test set
         assert isinstance(true_data_files, list)
         assert isinstance(false_data_files, list)
 
-        self.test_dataset.add_true_files(true_data_files)
-        self.test_dataset.add_false_files(false_data_files)
+        test_set = self.test_sets[test_set_idx]
+        
+        test_set.add_true_files(true_data_files)
+        test_set.add_false_files(false_data_files)
+        logging.info(f'ADDING TEST DATA')
         logging.info(
-            "Added {} true files and {} false files to test dataset.".format(
-                len(true_data_files), len(false_data_files))
+            "Test dataset {}: added {} true files and {} false files.".format(
+                test_set_idx, len(true_data_files), len(false_data_files))
             )
         logging.info(
-            "Actual test set size: {} true files and {} false files".format(
-                len(self.test_dataset.true_data), len(self.test_dataset.false_data))
+            "  --num images: {} true images and {} false images.".format(
+                len(test_set.true_data), len(test_set.false_data))
             )
 
-    def train(self):
+        #self.full_set.add_true_files(true_data_files)
+        #self.full_set.add_false_files(false_data_files)
+        
+
+    def train(self, save_ftr_dist=True):
         # train attention set
         self.classifier.train(epochs = self.classifier_train_epochs, 
-                              save_ftr_distribution = True)
+                              save_ftr_distribution = save_ftr_dist)
+
 
     def stats_experiment(self):
         # get stats for training dataset: 
@@ -95,8 +127,8 @@ class ClassifierExperiment():
         mean, sd, n = train_stats
         
         # get stats for "new" states: calculate sample confidence
-        test_positive_confidence = self.classifier.sample_confidence(self.test_dataset.true_data)
-        test_negative_confidence = self.classifier.sample_confidence(self.test_dataset.false_data)
+        test_positive_confidence = self.classifier.sample_confidence(self.test_sets[0].true_data)
+        test_negative_confidence = self.classifier.sample_confidence(self.test_sets[0].false_data)
         
         logging.info("***TRAINING FEATURE STATS***")
         logging.info('ftr mean shape: ' + str(mean.shape))
@@ -121,5 +153,152 @@ class ClassifierExperiment():
         logging.info('test negative confidence n: ' + str(len(test_negative_confidence)))
 
 
-    def classifier_weighted_experiment(self):
+    def predict(self, x): 
+        with torch.no_grad():
+            # init full_predictions tensor
+            _,_,first_votes,_ = self.classifier.vote(x[0])
+            full_predictions = torch.zeros((len(x), len(first_votes)))
+            vote_predictions = torch.zeros((len(x)))
+            if self.use_gpu:
+                full_predictions = full_predictions.to("cuda")
+                vote_predictions = vote_predictions.to("cuda")
+
+            # fill full_predictions tensor
+            for i in range(len(x)):
+                this_vote,_,this_votes,_ = self.classifier.vote(x[i])
+                full_predictions[i] = this_votes
+                vote_predictions[i] = this_vote
+            
+        return full_predictions, vote_predictions
+
+    def val_accuracy(self, val_true, val_false):
+        with torch.no_grad():
+            val_data = torch.cat((val_true, val_false))
+            labels_true = torch.cat((torch.ones(len(val_true)), torch.zeros(len(val_false))))
+            
+            if self.use_gpu:
+                val_data = val_data.to("cuda")
+                labels_true = labels_true.to("cuda")
+
+            labels_pred, vote_pred = self.predict(val_data) # have shape (num_samples, num_heads), want accu for each head 
+            logging.info('===VAL ACCURACY (FULL)===')           
+            full_acc = self.pred_accuracy(labels_pred, labels_true)
+            logging.info('===VAL ACCURACY (VOTE)===')       
+            vote_acc = self.pred_accuracy(vote_pred, labels_true)
+            #print("Val accuracy for each attention: ", full_acc)
+            #print("Val accuracy for voted prediction: ", vote_acc)
+        return full_acc, vote_acc
+
+    def pred_accuracy(self, labels_pred, labels_true):
+        # pred labels can be (num_samples, num_heads)
+        # true labels can be (num_samples)
+        with torch.no_grad():
+
+            if labels_pred.dim() > 1: 
+                labels_true_expanded = labels_true.unsqueeze(1).expand_as(labels_pred)
+                
+                TP = ((labels_pred == 1) & (labels_true_expanded == 1)).float().sum(dim=0)
+                FP = ((labels_pred == 1) & (labels_true_expanded == 0)).float().sum(dim=0)
+                TN = ((labels_pred == 0) & (labels_true_expanded == 0)).float().sum(dim=0)
+                FN = ((labels_pred == 0) & (labels_true_expanded == 1)).float().sum(dim=0)
+                
+                acc = (labels_pred == labels_true_expanded).float().mean(dim=0)
+                precision = TP / (TP + FP)
+                recall = TP / (TP + FN)
+                
+                logging.info(f'Accuracy: {acc} \nPrecision: {precision} \nRecall: {recall}')
+            else:
+                labels_pred = labels_pred.squeeze()
+                
+                TP = ((labels_pred == 1) & (labels_true == 1)).float().sum()
+                FP = ((labels_pred == 1) & (labels_true == 0)).float().sum()
+                TN = ((labels_pred == 0) & (labels_true == 0)).float().sum()
+                FN = ((labels_pred == 0) & (labels_true == 1)).float().sum()
+                
+                acc = (labels_pred == labels_true).float().mean()
+                precision = TP / (TP + FP)
+                recall = TP / (TP + FN)
+                
+                logging.info(f'\nAccuracy: {acc} \nPrecision: {precision} \nRecall: {recall}')
+        return acc
+    
+    def add_unlabeled_data(self, unlabeled_test_set):
+        # generate labels for the given test set
+        # then use data w/ generated lebel to fine-tunes the classifier 
+        # NOTE: fine tune w/ full dataset (train + test generated labels)
+        with torch.no_grad():
+            labels_pred, vote_pred = self.predict(unlabeled_test_set)
+            vote_pred = vote_pred.to("cpu").numpy()
+            #test_true = list(unlabeled_test_set[vote_pred == 1])
+            #test_false = list(unlabeled_test_set[vote_pred == 0])
+            test_true = [torch.from_numpy(img).float() for img in unlabeled_test_set[vote_pred == 1]]
+            test_false = [torch.from_numpy(img).float() for img in unlabeled_test_set[vote_pred == 0]]
+            
+            self.classifier.add_data(test_true, test_false)
+            #self.full_set.add_true_files(test_true)
+            #self.full_set.add_false_files(test_false)
+        
+        
+    def progressive_train(self, unlabeled_test_set, labels_actual):
+        # continue training/finetuning by generating labels and use for train
+        if self.use_gpu:
+            labels_actual = labels_actual.to("cuda")
+            
+        self.add_unlabeled_data(unlabeled_test_set)
+
+        with torch.no_grad():
+            labels_pred, vote_pred = self.predict(unlabeled_test_set)
+            logging.info('===TEST METRICS, BEFORE TRAIN ===')
+            logging.info('FULL')
+            self.pred_accuracy(labels_pred, labels_actual)
+            logging.info('VOTE')
+            self.pred_accuracy(vote_pred, labels_actual)
+
+        # NOTE: retrain keeps updating ftr distribution (which will be used to compute next sample confidence)
+        #self.train() 
+        self.train(False) # TODO: not just use sample confidence, but also use pred probability to measure Confidence.
+
+        with torch.no_grad():
+            labels_pred, vote_pred = self.predict(unlabeled_test_set)
+            logging.info(f'===TEST METRICS, AFTER TRAIN ===')
+            logging.info('FULL')
+            self.pred_accuracy(labels_pred, labels_actual)
+            logging.info('VOTE')
+            self.pred_accuracy(vote_pred, labels_actual)
+
+
+    def progressive_train_testing(self, test_pos, test_neg, unlabeled_test_set, labels_actual):
+        # continue training/finetuning by generating labels and use for train
+        if self.use_gpu:
+            labels_actual = torch.from_numpy(labels_actual)
+            labels_actual = labels_actual.to("cuda")
+        self.add_train_data(test_pos, test_neg)
+
+        with torch.no_grad():
+            labels_pred, vote_pred = self.predict(unlabeled_test_set)
+            logging.info('===TEST METRICS, BEFORE TRAIN ===')
+            logging.info('FULL')
+            self.pred_accuracy(labels_pred, labels_actual)
+            logging.info('VOTE')
+            self.pred_accuracy(vote_pred, labels_actual)
+            
+        # NOTE: retrain keeps updating ftr distribution (which will be used to compute next sample confidence)
+        #self.train() 
+        self.train(False)
+
+        with torch.no_grad():
+            labels_pred, vote_pred = self.predict(unlabeled_test_set)
+            logging.info(f'===TEST METRICS, AFTER TRAIN ===')
+            logging.info('FULL')
+            self.pred_accuracy(labels_pred, labels_actual)
+            logging.info('VOTE')
+            self.pred_accuracy(vote_pred, labels_actual)
+
+
+    def retrain(self):
+        # reset weights and train 
+        #self.classifier.reset_weights() #TODO: reset weights function
+        pass
+
+
         
--- experiments/classifier/minigrid_confidence_weighted_loss.py
+++ experiments/classifier/minigrid_confidence_weighted_loss.py
@@ -1,24 +1,30 @@
+from cgi import test
+from pathlib import Path
+import logging
 import argparse
 import re
-#from experiments.classifier.core.class_experiment import ClassifierExperiment
-from . import ClassifierExperiment
-from portable.utils.utils import load_gin_configs
-from pathlib import Path
+
 import numpy as np
+import torch
+from portable.utils.utils import load_gin_configs
+from . import ClassifierExperiment
+#from experiments.classifier.core.class_experiment import ClassifierExperiment
+
 
 '''
 To run this experiment, use the following command:
-python -m experiments.classifier.minigrid_confidence_weighted_loss --task 'blue red initiation' --base_dir experiments/classifier/ --seed 0 --config_file configs/confidence_weighted_loss.gin
+python -m experiments.classifier.minigrid_confidence_weighted_loss --task openpurpledoor_doorpurple initiation --base_dir experiments/classifier/ --exp_seed 0 --train_seed 0 --test_seed 1 2 --config_file configs/confidence_weighted_loss.gin
 '''
 
-# TODO: set up arg parser and config file, if needed
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--task", type=str, required=True, help='For getbluekey_doorred initiation set, write \'blue red initiation\'')
+    parser.add_argument("--task", nargs='+', type=str, required=True, help='For openpurpledoor_doorpurple initiation set, write \'openpurpledoor_doorpurple initiation\'')
 
     parser.add_argument("--base_dir", type=str, required=True)
-    parser.add_argument("--seed", type=int, required=True)
+    parser.add_argument("--exp_seed", type=int, required=True)
+    parser.add_argument("--train_seed", nargs='+', type=int, required=True)
+    parser.add_argument("--test_seed",  nargs='+', type=int, required=True)
     parser.add_argument("--config_file", nargs='+', type=str, required=True)
     parser.add_argument("--gin_bindings", default=[], help='Gin bindings to override the values' + 
 	    ' set in the config files (e.g. "DQNAgent.epsilon_train=0.1",' +
@@ -28,50 +34,107 @@ if __name__ == "__main__":
     load_gin_configs(args.config_file, args.gin_bindings)
 
     task = args.task
-    key_color, door_color, init_or_term = re.split('[ ,_]+', task)[:3]
+    #task_name, init_or_term = re.split('[ ,]+', task)[:2]
+    task_name, init_or_term = task
     
-    def task_to_pattern(key_color, door_color, init_or_term, seed, positive_or_negative):
-        pattern = rf'8x8.*{re.escape(key_color)}.*door{re.escape(door_color)}.*[{re.escape(str(seed))}].*{re.escape(init_or_term)}.*{re.escape(positive_or_negative)}\.npy'
+    def task_to_pattern(task_name, init_or_term, seed, positive_or_negative):
+        pattern = rf'.*8x8.*{re.escape(task_name)}.*{re.escape(str(seed))}.*{re.escape(init_or_term)}.*{re.escape(positive_or_negative)}\.npy'
         return pattern
-
 
     dataset_folder = Path('resources/minigrid_images/')
     all_images = list(dataset_folder.glob('*.npy'))
     #   rf'{re.escape(A)}[ ,_]+{re.escape(B)}[ ,_]+{re.escape(C)}'
     #   adv_doorkey_8x8_getbluekey_doorblue_0_initiation_negative.npy
-    train_positive_regex = task_to_pattern(key_color, door_color, init_or_term, 0, 'positive')
-    train_negative_regex = task_to_pattern(key_color, door_color, init_or_term, 0, 'negative')
-    test_positive_regex = task_to_pattern(key_color, door_color, init_or_term, 12, 'positive')
-    test_negative_regex = task_to_pattern(key_color, door_color, init_or_term, 12, 'negative')
+    train_pos_regex = []
+    train_neg_regex = []
+    test_pos_regex = []
+    test_neg_regex = []
+    for i in range(len(args.train_seed)):
+        train_pos_regex.append(task_to_pattern(task_name, init_or_term, args.train_seed[i], 'positive'))
+        train_neg_regex.append(task_to_pattern(task_name, init_or_term, args.train_seed[i], 'negative'))
+    for i in range(len(args.test_seed)):
+        test_pos_regex.append(task_to_pattern(task_name, init_or_term, args.test_seed[i], 'positive'))
+        test_neg_regex.append(task_to_pattern(task_name, init_or_term, args.test_seed[i], 'negative'))
+    
+    train_pos_files = []
+    train_neg_files = []
+    test_pos_files = []
+    test_neg_files = []
+    for i in range(len(train_pos_regex)):
+        train_pos_files.append([f for f in all_images if re.match(train_pos_regex[i], str(f))])
+        train_neg_files.append([f for f in all_images if re.match(train_neg_regex[i], str(f))])
+    for i in range(len(test_neg_regex)):
+        test_pos_files.append([f for f in all_images if re.match(test_pos_regex[i], str(f))])
+        test_neg_files.append([f for f in all_images if re.match(test_neg_regex[i], str(f))])  
 
-    train_positive_files = [f for f in all_images if re.match(train_positive_regex, str(f))]
-    train_negative_files = [f for f in all_images if re.match(train_negative_regex, str(f))]
-    test_positive_files = [f for f in all_images if re.match(test_positive_regex, str(f))]
-    test_pnegative_files = [f for f in all_images if re.match(test_negative_regex, str(f))]
-
-    #print("Train positive files: ", len(train_positive_files))
-    #print("Train negative files: ", len(train_negative_files))
-    #print("Test positive files: ", len(test_positive_files))
-    #print("Test negative files: ", len(test_pnegative_files))
+    print("Train sets: ", len(train_pos_files))
+    print("Test sets: ", len(test_pos_files))
 	
     experiment = ClassifierExperiment(base_dir=args.base_dir,
-				                      seed=args.seed)
-    print("Adding train data...")
-    experiment.add_train_data(train_positive_files, train_negative_files)
-    print("Positive data length: ", len(experiment.classifier.dataset.true_data))
-    print("False data length: ", len(experiment.classifier.dataset.false_data))
-    print("Priority false data length: ", len(experiment.classifier.dataset.priority_false_data))
+				                      seed=args.exp_seed)
 
-    print("Adding test data...")
-    experiment.add_test_data(test_positive_files, test_pnegative_files)
-    print("Positive data length: ", len(experiment.test_dataset.true_data))
-    print("False data length: ", len(experiment.test_dataset.false_data))
-    print("Priority false data length: ", len(experiment.test_dataset.priority_false_data))
+    logging.info(f'===EXPERIMENT PARAMS===')
+    logging.info(f' --TASK: {task_name} ({init_or_term})')
+    logging.info(f' --EXPERIMENT SEED: {args.exp_seed}')
+    logging.info(f' --TRAIN SEEDS: {args.train_seed}')
+    logging.info(f' --TEST SEEDS: {args.test_seed}')
+    logging.info(f' --CONFIDENCE: NOT updating when training on test sets')
+    logging.info(f' --SD cutoff: 4')
+    logging.info(f' --NOTE: Using progressive training.')
+    #logging.info(f' --NOTE: This is not test w/ perfect test labels. Not progressive training.')
+
     
-    print("Begin training...")
-    experiment.train()
+    print("***Adding Train & Val Images***") # train sets always all added at once
+    for i in range(len(train_pos_files)):
+        experiment.add_train_data(train_pos_files[i], train_neg_files[i])
+    """print("TRAIN positive img: ", len(experiment.classifier.dataset.true_data))
+    print("TRAIN false img: ", len(experiment.classifier.dataset.false_data))
+    print("VAL positive img: ", len(experiment.classifier.dataset.validate_indicies_true))
+    print("VAL false img: ", len(experiment.classifier.dataset.validate_indicies_false))"""
+
+
+    print("***Adding Testsets Data***")
+    for i in range(len(test_pos_files)):
+        experiment.add_test_data(i, test_pos_files[i], test_neg_files[i])
+
+    
+    print("***Begin training classifier***")
+    experiment.train(True)
     print("Classifier trained.")
+    logging.info('classifier trained')
+
+    print("==================VALIDATION 0==================")
+    logging.info("==================VALIDATION 0==================")
+    print("After train on set 0, check val accuracy")
+
+    val_true = experiment.classifier.dataset.true_data[experiment.classifier.dataset.validate_indicies_true]
+    val_false = experiment.classifier.dataset.false_data[experiment.classifier.dataset.validate_indicies_false]
+    logging.info(f'Val True shape : {val_true.shape}')
+    logging.info(f'Val False shape: {val_false.shape}')
+
+    #results = experiment.predict(val_true)
+    experiment.val_accuracy(val_true, val_false)
+
+
+    print("==================VALIDATION 1==================")
+    logging.info("==================VALIDATION 1==================")
+
+    test_set1 = np.concatenate((experiment.test_sets[0].true_data, experiment.test_sets[0].false_data))
+    labels1 = np.concatenate((np.array([1]*len(experiment.test_sets[0].true_data)), np.array([0]*len(experiment.test_sets[0].false_data))))
+
+    experiment.progressive_train(test_set1, torch.from_numpy(labels1))
+    #experiment.progressive_train_testing(test_pos_files[0], test_neg_files[0], test_set1, labels1)
     
-    print("Begin stats...")
-    experiment.stats_experiment()
-    print("Stats experiment complete.")
+    experiment.val_accuracy(val_true, val_false)
+
+    print("==================VALIDATION 2==================")
+    logging.info("==================VALIDATION 2==================")
+    test_set2 = np.concatenate((experiment.test_sets[1].true_data, experiment.test_sets[1].false_data))
+    labels2 = np.concatenate((np.array([1]*len(experiment.test_sets[1].true_data)), np.array([0]*len(experiment.test_sets[1].false_data))))
+
+    experiment.progressive_train(test_set2, torch.from_numpy(labels2))
+    #experiment.progressive_train_testing(test_pos_files[1], test_neg_files[1], test_set2, labels2)
+
+    experiment.val_accuracy(val_true, val_false)
+    
+    
