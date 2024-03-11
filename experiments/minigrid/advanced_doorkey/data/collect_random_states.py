ff8c01223cba46f79c53ed2657f6b07d4f47f522
Bingnan Huo <98042550+BingnanHuo@users.noreply.github.com>
2024-03-11 16:13:53 -0400

    fix collect random states

--- experiments/minigrid/advanced_doorkey/data/collect_random_states.py
+++ experiments/minigrid/advanced_doorkey/data/collect_random_states.py
@@ -11,19 +11,33 @@ from experiments.minigrid.advanced_doorkey.core.policy_train_wrapper import \
 from experiments.minigrid.utils import (environment_builder,
                                         factored_environment_builder)
 
-
-seed = 11
-
+if __name__ == '__main__':
 
     def collect_seed(seed, initiation=None, termination=None):
         colours = ["red", "green", "blue", "purple", "yellow", "grey"]
         
-        obs, _ = env.reset(random_start=True,
-                        keep_colour=colour,
-                        agent_reposition_attempts=repos_attempts)
-        
-        states.append(obs.numpy())
-
+        for colour in colours:
+            states = []
+            for _ in range(2000):
+                repos_attempts = np.random.randint(low=0, high=1000)
+                env = factored_environment_builder('AdvancedDoorKey-8x8-v0',
+                                                    seed=seed)
+                env = AdvancedDoorKeyPolicyTrainWrapper(env,
+                                                        door_colour=colour,
+                                                        image_input=False)
+                
+                if (initiation is True) or (termination is False): # init pos, term neg
+                    obs, _ = env.reset(random_start=True, 
+                                       keep_colour=colour,
+                                       agent_reposition_attempts=repos_attempts)
+                elif (initiation is False) or (termination is True): # init neg, term pos
+                    obs, _ = env.reset(random_start=True, 
+                                       pickup_colour=colour,
+                                       agent_reposition_attempts=repos_attempts)
+                else:
+                    raise ValueError("initiation and termination cannot be the same")
+                
+                states.append(obs.numpy())
 
             states = np.array(states)
 
@@ -39,16 +53,12 @@ seed = 11
             print(f"Saved seed {seed}, colour {colour}, initiation {initiation}, termination {termination}")
             print(f"states shape: {states.shape}")
 
-    np.save("resources/factored_minigrid_images/adv_doorkey_8x8_v2_get{}key_door{}_{}_1_termination_negative.npy".format(colour, colour, seed)
-            , states)
-
 
     # multiprocessing
     seeds = [0,1,2,3,4,5,6,7,8,9,10,11]
 
     with Pool() as p:
         p.map(partial(collect_seed, termination=True), seeds)
-
 
     with Pool() as p:
         p.map(partial(collect_seed, termination=False), seeds)
