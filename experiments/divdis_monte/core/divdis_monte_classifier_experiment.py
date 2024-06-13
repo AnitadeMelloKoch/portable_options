72f8405b8bbcaf0fa3803a256ef429b922e9cdb9
Bingnan Huo <bingnan_huo@brown.edu>
2024-06-13 15:53:44 -0400

    fix monte classifier exp

--- experiments/divdis_monte/core/divdis_monte_classifier_experiment.py
+++ experiments/divdis_monte/core/divdis_monte_classifier_experiment.py
@@ -79,7 +79,7 @@ class MonteDivDisClassifierExperiment():
                                            num_classes=classifier_num_classes,
                                            diversity_weight=classifier_diversity_weight,
                                            l2_reg_weight=classifier_l2_reg_weight,
-                                           model_name='classifier_model_name'
+                                           model_name=classifier_model_name
                                            )
         #self.classifier.dataset.set_transform_function(transform)
 
