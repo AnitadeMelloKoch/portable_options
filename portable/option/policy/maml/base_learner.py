a40f5599f6931da73a9baa0531499f4b6d1c8f21
Anita de Mello Koch <demellokochanita@gmail.com>
2024-11-19 11:34:51 -0500

    [WIP] add base of maml learner

---
+++ portable/option/policy/maml/base_learner.py
@@ -0,0 +1,19 @@
+# taken from https://github.com/learnables/learn2learn/tree/master
+
+from torch import nn
+
+
+class BaseLearner(nn.Module):
+
+    def __init__(self, module=None):
+        super(BaseLearner, self).__init__()
+        self.module = module
+
+    def __getattr__(self, attr):
+        try:
+            return super(BaseLearner, self).__getattr__(attr)
+        except AttributeError:
+            return getattr(self.__dict__['_modules']['module'], attr)
+
+    def forward(self, *args, **kwargs):
+        return self.module(*args, **kwargs)
