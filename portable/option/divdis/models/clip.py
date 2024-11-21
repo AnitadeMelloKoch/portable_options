57f8313c2b1f32ac33d3e3592ff756887e89d1c8
yyang239 <yyang239@YuechuanYang>
2024-11-21 12:36:58 -0500

    add linear layer

--- portable/option/divdis/models/clip.py
+++ portable/option/divdis/models/clip.py
@@ -19,8 +19,10 @@ class Clip(nn.Module):
         # Custom layers for predictions
         self.model = nn.ModuleList([
             nn.Sequential(
-                nn.Linear(embedding_dim, 128),
+                nn.Linear(embedding_dim, 256),
                 nn.ReLU(),
+                nn.Linear(256, 128),
+                nn.ReLU(),                
                 nn.Linear(128, 64),
                 nn.ReLU(),
                 nn.Linear(64, num_classes)
