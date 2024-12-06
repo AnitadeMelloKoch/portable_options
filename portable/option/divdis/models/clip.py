f5286cde096ba7827a413eda6c6201deefa48beb
yyang239 <yyang239@YuechuanYang>
2024-12-06 10:25:43 -0500

    add wrapper 12/6

--- portable/option/divdis/models/clip.py
+++ portable/option/divdis/models/clip.py
@@ -22,6 +22,10 @@ class Clip(nn.Module):
                 nn.Linear(64, num_classes)
             ) for _ in range(num_heads)
         ]).to(device)
+        self.full_model = nn.ModuleList([
+            nn.Sequential(self.clip_model, classification_head)
+            for classification_head in self.model
+        ]).to(device)
         self.num_heads = num_heads
         self.num_classes = num_classes
     def forward(self, images):
