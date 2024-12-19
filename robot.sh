7a93e38b140d5288faca47d8ddac347311d0ff81
Kyle Lee <kylelee@cis1b1zs2j3.ad.brown.edu>
2024-12-18 19:27:13 -0800

    test out robot

---
+++ robot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH --gres gpu:2
+#SBATCH --mem=64G
+#SBATCH --time=1-00:00:00
+#SBATCH --error=<clip_dog_eval>.%J.err
+#SBATCH --output=<clip_dog_eval>.%J.out
+#SBATCH --mail-type=END
+#SBATCH --mail-user=kyle_k_lee@brown.edu
+module load miniconda3/23.11.0s
+source /oscar/runtime/software/external/miniconda3/23.11.0/etc/profile.d/conda.sh
+conda activate divlis
+python -m experiments.classifier.jaco_handle_experiment --base_dir runs/ --seed 100 --config_file configs/dog_evaluator_classifier.gin
