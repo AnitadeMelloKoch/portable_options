82590ce4de05dab75f090e2f27865c4e706a8249
Kyle Lee <kylelee@cis1b1zs2j3.ad.brown.edu>
2025-01-22 23:19:19 -0800

    change to robot structure

--- robot.sh
+++ robot.sh
@@ -10,4 +10,4 @@
 module load miniconda3/23.11.0s
 source /oscar/runtime/software/external/miniconda3/23.11.0/etc/profile.d/conda.sh
 conda activate divlis
-python -m experiments.classifier.jaco_handle_experiment --base_dir runs/ --seed 100 --config_file configs/dog_evaluator_classifier.gin
+python -m experiments.classifier.jaco_handle_experiment --base_dir runs/ --seed 100 --config_file configs/classifier_jaco_handle.gin
