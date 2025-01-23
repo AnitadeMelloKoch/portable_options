298f3a5d5d3b87248f7848fe3ad004a98ae20e21
yyang239 <yyang239@YuechuanYang>
2025-01-22 22:08:03 -0500

    change back to clip curr

--- portable/option/divdis/divdis_classifier.py
+++ portable/option/divdis/divdis_classifier.py
@@ -13,7 +13,7 @@ from portable.option.divdis.models.monte_cnn import MonteCNN
 from portable.option.divdis.divdis import DivDisLoss
 from portable.option.divdis.models.yolo import YOLOEnsemble
 # from portable.option.divdis.models.clip import Clip
-from portable.option.divdis.models.clip_prev import Clip
+from portable.option.divdis.models.clip import Clip
 logger = logging.getLogger(__name__)
 MODEL_TYPE = [
     "one_head_mlp",
