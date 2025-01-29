080faf670c6aecec2fb0b4320d4d1780abe4de64
yyang239 <yyang239@YuechuanYang>
2025-01-29 09:40:45 -0500

    transpose 0 2 1

--- evaluation/evaluators/divdis_evaluator_classifier.py
+++ evaluation/evaluators/divdis_evaluator_classifier.py
@@ -133,9 +133,9 @@ class DivDisEvaluatorClassifier():
                 attr = self.integrated_gradients[head_idx].attribute(
                     image,
                     target=label
-                ).squeeze().cpu().detach().numpy().transpose(1, 2, 0) # (H, W, C)
+                ).squeeze().cpu().detach().numpy().transpose(0, 2, 1) # (H, W, C)
                 
-                display_image = image.squeeze().detach().cpu().numpy().transpose(1, 2, 0) # (H, W, C)
+                display_image = image.squeeze().detach().cpu().numpy().transpose(0, 2, 1) # (H, W, C)
                 print("Display Image Shape: ", display_image.shape)
                 print("Attribution Shape: ", attr.shape)
 
