48a2eac5f5927072532596cadf708125ecc730da
yyang239 <yyang239@YuechuanYang>
2025-01-28 22:16:51 -0500

    expand image size

--- portable/option/memory/imagenet_dataset.py
+++ portable/option/memory/imagenet_dataset.py
@@ -65,10 +65,9 @@ def load_annotations_from_folder(folder):
     return annotations, filenames
 # Define any image transformations you want (e.g., resizing)
 image_transform = transforms.Compose([
-    transforms.Resize((224, 224)),  
+    transforms.Resize((224, 224)),  # Resize to 224x224
     transforms.ToTensor(),  # Convert to tensor
 ])
-
 # Load the images and annotations for Chihuahuas and Spaniels
 chihuahua_images, chihuahua_filenames = load_images_from_folder(chihuahua_image_dir, transform=image_transform)
 spaniel_images, spaniel_filenames = load_images_from_folder(spaniel_image_dir, transform=image_transform)
@@ -86,7 +85,6 @@ for img, filename in zip(spaniel_images, spaniel_filenames):
     np.save(img_save_path, img)
     print(f"Saved Spaniel image: {img_save_path}")
 print("All images and annotations saved individually.")
-
 
 
 
