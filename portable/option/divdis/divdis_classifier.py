36b5fcbe5d2ce1551a570353c3e8b3122638d15a
Kyle Lee <kylelee@eng-hplt-12.ad.brown.edu>
2025-05-01 19:46:24 -0400

    again

--- portable/option/divdis/divdis_classifier.py
+++ portable/option/divdis/divdis_classifier.py
@@ -14,7 +14,7 @@ from portable.option.divdis.models.minigrid_cnn_16x16 import MinigridCNN16x16
 from portable.option.divdis.models.monte_cnn import MonteCNN
 from portable.option.divdis.divdis import DivDisLoss
 from portable.option.divdis.models.yolo import YOLOEnsemble
-from portable.option.divdis.models.clip import Clip
+# from portable.option.divdis.models.clip import Clip
 logger = logging.getLogger(__name__)
 MODEL_TYPE = [
     "one_head_mlp",
@@ -114,9 +114,9 @@ class DivDisClassifier():
         elif self.model_name == "yolo":
             self.classifier = YOLOEnsemble(num_classes=self.num_classes,
                                                num_heads= self.head_num)
-        elif self.model_name == "clip":
-            self.classifier = Clip(num_classes=self.num_classes,
-                                               num_heads= self.head_num)
+        # elif self.model_name == "clip":
+        #     self.classifier = Clip(num_classes=self.num_classes,
+        #                                        num_heads= self.head_num)
         else:
             raise ValueError("model_name must be one of {}".format(MODEL_TYPE))
         self.classifier.to(self.device)
